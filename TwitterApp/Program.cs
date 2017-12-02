5fac2731d67c16b7e190d678e473087df0a0a69d
lutfiqaraman <lutfiqaraman@gmail.com>
2017-12-02 19:19:32 +0200

    Done

--- TwitterApp/Program.cs
+++ TwitterApp/Program.cs
@@ -11,21 +11,27 @@ namespace TwitterApp
     {
         static void Main(string[] args)
         {
-            var CONSUMER_KEY = "type your CONSUMER_KEY here";
-            var CONSUMER_SECRET = "type your CONSUMER_SECRET here";
-            var ACCESS_TOKEN = "type your ACCESS_TOKEN here";
-            var ACCESS_TOKEN_SECRET = "type your ACCESS_TOKEN_SECRET here";
+            var CONSUMER_KEY = "your CONSUMER_KEY is here";
+            var CONSUMER_SECRET = "your CONSUMER_SECRET is here";
+            var ACCESS_TOKEN = "your ACCESS_TOKEN is here";
+            var ACCESS_TOKEN_SECRET = "your ACCESS_TOKEN_SECRET is here";
 
             Auth.SetUserCredentials(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
             var user = User.GetAuthenticatedUser();
 
-            Console.WriteLine("Please write your tweet");
+            Console.WriteLine("Please write your tweet:");
             string tweetBody = Console.ReadLine();
 
             if (tweetBody.Length <= 140)
+            {
                 Tweet.PublishTweet(tweetBody);
+                Console.WriteLine("Published successfully");
+            }  
             else
+            {
                 Console.WriteLine("Faild to publish, your tweet is more than 140 characters");
+            }
+                
 
             Console.ReadKey();
 
